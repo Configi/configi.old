4938887fcfe052fad83189bd6378f6da94bfed1e
Eduardo Tongson <propolice@gmail.com>
2015-02-04 15:33:32 +0000

    Update cross-compiling helper scripts to use a configurable SDK location and for the aux->scripts change.

--- scripts/luac.sh
+++ scripts/luac.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-/usr/bin/qemu-mipsel -L /sda3/work/sdk/OpenWrt-SDK-brcm47xx-for-linux-x86_64-gcc-4.8-linaro_uClibc-0.9.33.2/staging_dir/toolchain-mipsel_mips32_gcc-4.8-linaro_uClibc-0.9.33.2 /sda3/work/musl/root/fossil/configi.src/bin/luac $@
+SDK=""
+/usr/bin/qemu-mipsel -L $SDK/OpenWrt-SDK-brcm47xx-for-linux-x86_64-gcc-4.8-linaro_uClibc-0.9.33.2/staging_dir/toolchain-mipsel_mips32_gcc-4.8-linaro_uClibc-0.9.33.2 bin/luac $@
