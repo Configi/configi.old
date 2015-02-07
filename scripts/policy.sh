65a3d9eada0e31a627f12c8d7fb8db5bb6b5fc62
Eduardo Tongson <propolice@gmail.com>
2015-02-07 01:46:34 +0000

    scripts/policy.sh: Add usage information and sanity checks.

--- scripts/policy.sh
+++ scripts/policy.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+USAGE="Usage: $0 DIRECTORY"
+[ "$1" = "-h" ] && { echo $USAGE; exit 0; }
+[ -d "lib/policy" ] || { echo "ERROR: Should be run in the top-level directory of configi." ; exit 1; }
+[ $# -eq 0 ] && { echo "ERROR: Directory argument missing. $USAGE" ; exit 1; }
 [ -d "lib/policy/src" ] || mkdir "lib/policy/src"
 SRC="$PWD/lib/policy/src/policy.lua"
 pushd "$1"
