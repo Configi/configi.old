646bdc776abebf382cdafb7a1a82bb6d8e463246
Eduardo Tongson <propolice@gmail.com>
2015-04-26 11:22:10 +0000

    cfg: namespacing changes.

--- bin/cfg.lua
+++ bin/cfg.lua
@@ -1,21 +1,20 @@
 local version = "Configi 0.9.7"
 local arg = arg
-local Lua = {
-  exit = os.exit,
+local os, string = os, string
+local core = {
   next = next,
   tostring = tostring,
-  format = string.format,
   collectgarbage = collectgarbage
 }
-local Lib = require"configi"
-local Lc = require"cimicida"
-local Lcli = Lib.cli
-local Punistd = require"posix.unistd"
-local Psignal = require"posix.signal"
-local Pstat = require"posix.sys.stat"
-local Psyslog = require"posix.syslog"
-local Psystime = require"posix.sys.time"
-local Px = require"px"
+local cfg = require"configi"
+local c = require"cimicida"
+local cli = cfg.cli
+local unistd = require"posix.unistd"
+local signal = require"posix.signal"
+local sysstat = require"posix.sys.stat"
+local syslog = require"posix.syslog"
+local systime = require"posix.sys.time"
+local px = require"px"
 local inotify = require"inotify"
 local t1
 local ENV = {}
@@ -23,41 +22,41 @@ _ENV = ENV
 
 while true do
   local handle, wd
-  local source, hsource, runenv, opts = Lcli.opt(arg, version)
+  local source, hsource, runenv, opts = cli.opt(arg, version)
   ::RUN::
-  t1 = Psystime.gettimeofday()
-  local R, M = Lcli.try(source, hsource, runenv)
+  t1 = systime.gettimeofday()
+  local R, M = cli.try(source, hsource, runenv)
   if not R.failed and not R.repaired then
     R.kept = true
   end
   if opts.debug then
-    Lc.printf("------------\n")
+    c.printf("------------\n")
     if R.kept then
-      Lc.printf("Kept: %s\n", R.kept)
+      c.printf("Kept: %s\n", R.kept)
     elseif R.repaired then
-      Lc.printf("Repaired: %s\n", R.repaired)
+      c.printf("Repaired: %s\n", R.repaired)
     elseif R.failed then
-      Lc.printf("Failed: %s\n", R.failed)
-      Lc.errorf("Failed!\n")
+      c.printf("Failed: %s\n", R.failed)
+      c.errorf("Failed!\n")
     end
-    local t2 = Px.difftime(Psystime.gettimeofday(), t1)
-    t2 = Lua.format("%s.%s", Lua.tostring(t2.sec), Lua.tostring(t2.usec))
+    local t2 = px.difftime(systime.gettimeofday(), t1)
+    t2 = string.format("%s.%s", core.tostring(t2.sec), core.tostring(t2.usec))
     if t2 == 0 or t2 == 1.0 then
-      Lc.printf("Finished run in %.f second\n", 1.0)
+      c.printf("Finished run in %.f second\n", 1.0)
     else
-      Lc.printf("Finished run in %.f seconds\n", t2)
+      c.printf("Finished run in %.f seconds\n", t2)
     end
   else
     if R.failed then
-      Lua.exit(1)
+      core.exit(1)
     end
   end
   if opts.daemon then
-    if Punistd.geteuid() == 0 then
-      if Pstat.stat("/proc/self/oom_score_adj") then
-        Px.fwrite("/proc/self/oom_score_adj", "-1000")
+    if unistd.geteuid() == 0 then
+      if sysstat.stat("/proc/self/oom_score_adj") then
+        px.fwrite("/proc/self/oom_score_adj", "-1000")
       else
-        Px.fwrite("/proc/self/oom_adj", "-1000")
+        px.fwrite("/proc/self/oom_adj", "-1000")
       end
     end
     handle = inotify.init()
@@ -65,16 +64,16 @@ while true do
     local bail = function(sig)
       handle:rmwatch(wd)
       handle:close()
-      Lib.LOG(opts.syslog, opts.log, Lua.format("Caught signal %s. Exiting.", Lua.tostring(sig)), Psyslog.LOG_ERR)
-      Lua.exit(255)
+      cfg.LOG(opts.syslog, opts.log, string.format("Caught signal %s. Exiting.", core.tostring(sig)), syslog.LOG_ERR)
+      core.exit(255)
     end
-    Psignal.signal(Psignal.SIGINT, bail)
-    Psignal.signal(Psignal.SIGTERM, bail)
+    signal.signal(signal.SIGINT, bail)
+    signal.signal(signal.SIGTERM, bail)
     handle:read()
-    Lua.collectgarbage()
+    core.collectgarbage()
   elseif opts.periodic then
-    Punistd.sleep(opts.periodic)
-    Lua.collectgarbage()
+    unistd.sleep(opts.periodic)
+    core.collectgarbage()
     goto RUN
   else
     break
