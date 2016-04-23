2ec683fc54ef2f61ad9dfd2e61f45bbcb5a0e5f6
Eduardo Tongson <propolice@gmail.com>
2016-04-23 04:58:31 -0400

    build: compilation rules for tests.

---
+++ aux/configi.mk
@@ -0,0 +1,13 @@
+testDEPS:= cwtest.lua cimicida.lua crc32.lua lib.lua factid.lua px.a factid_c.a posix.sys.stat.a posix.pwd.a posix.grp.a posix.unistd.a posix.errno.a posix.sys.wait.a posix.poll.a posix.fcntl.a posix.stdlib.a posix.syslog.a posix.dirent.a
+CLEAN+= clean_tests
+
+tests: $(LUA_A) $(CLUA_MODS)
+	$(CP) vendor/lua/cwtest.lua .
+	$(ECHOT) [LN] $@
+	$(LUA_T) $(LUASTATIC) test/tests.lua $(testDEPS) $(LUA_A) $(INCLUDES) $(CCWARN) $(CFLAGS) $(CCOPT) $(LDFLAGS) 
+	$(RM) $(RMFLAGS) cwtest.lua
+
+clean_tests:
+	$(RM) $(RMFLAGS) tests
+
+.PHONY: clean_tests
