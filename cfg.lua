4a538c95d61423f39ee5550e9c364be70c85ebec
Eduardo Tongson <propolice@gmail.com>
2016-08-13 04:44:25 -0400

    cfg: 2 to 4 space indention.

--- cfg.lua
+++ cfg.lua
@@ -17,62 +17,62 @@ local ENV = {}
 _ENV = ENV
 
 while true do
-  local handle, wd
-  local source, hsource, runenv, opts = cli.opt(arg, version)
-  ::RUN::
-  t1 = systime.gettimeofday()
-  local R, M = cli.try(source, hsource, runenv)
-  if not R.failed and not R.repaired then
-    R.kept = true
-  end
-  if opts.debug then
-    lib.printf("------------\n")
-    if R.kept then
-      lib.printf("Kept: %s\n", R.kept)
-    elseif R.repaired then
-      lib.printf("Repaired: %s\n", R.repaired)
-    elseif R.failed then
-      lib.printf("Failed: %s\n", R.failed)
-      lib.errorf("Failed!\n")
+    local handle, wd
+    local source, hsource, runenv, opts = cli.opt(arg, version)
+    ::RUN::
+    t1 = systime.gettimeofday()
+    local R, M = cli.try(source, hsource, runenv)
+    if not R.failed and not R.repaired then
+        R.kept = true
     end
-    local t2 = lib.difftime(systime.gettimeofday(), t1)
-    t2 = string.format("%s.%s", tostring(t2.sec), tostring(t2.usec))
-    if t2 == 0 or t2 == 1.0 then
-      lib.printf("Finished run in %.f second\n", 1.0)
+    if opts.debug then
+        lib.printf("------------\n")
+        if R.kept then
+            lib.printf("Kept: %s\n", R.kept)
+        elseif R.repaired then
+            lib.printf("Repaired: %s\n", R.repaired)
+        elseif R.failed then
+            lib.printf("Failed: %s\n", R.failed)
+            lib.errorf("Failed!\n")
+        end
+        local t2 = lib.difftime(systime.gettimeofday(), t1)
+        t2 = string.format("%s.%s", tostring(t2.sec), tostring(t2.usec))
+        if t2 == 0 or t2 == 1.0 then
+            lib.printf("Finished run in %.f second\n", 1.0)
+        else
+            lib.printf("Finished run in %.f seconds\n", t2)
+        end
     else
-      lib.printf("Finished run in %.f seconds\n", t2)
+        if R.failed then
+            os.exit(1)
+        end
     end
-  else
-    if R.failed then
-      os.exit(1)
+    if opts.daemon then
+        if unistd.geteuid() == 0 then
+            if sysstat.stat("/proc/self/oom_score_adj") then
+                lib.fdwrite("/proc/self/oom_score_adj", "-1000")
+            else
+                lib.fdwrite("/proc/self/oom_adj", "-1000")
+            end
+        end
+        handle = inotify.init()
+        wd = handle:addwatch(opts.script, inotify.IN_MODIFY, inotify.IN_ATTRIB)
+        local bail = function(sig)
+            handle:rmwatch(wd)
+            handle:close()
+            cfg.LOG(opts.syslog, opts.log, string.format("Caught signal %s. Exiting.", tostring(sig)), syslog.LOG_ERR)
+            os.exit(255)
+        end
+        signal.signal(signal.SIGINT, bail)
+        signal.signal(signal.SIGTERM, bail)
+        handle:read()
+        collectgarbage()
+    elseif opts.periodic then
+        unistd.sleep(opts.periodic)
+        collectgarbage()
+        goto RUN
+    else
+        break
     end
-  end
-  if opts.daemon then
-    if unistd.geteuid() == 0 then
-      if sysstat.stat("/proc/self/oom_score_adj") then
-        lib.fdwrite("/proc/self/oom_score_adj", "-1000")
-      else
-        lib.fdwrite("/proc/self/oom_adj", "-1000")
-      end
-    end
-    handle = inotify.init()
-    wd = handle:addwatch(opts.script, inotify.IN_MODIFY, inotify.IN_ATTRIB)
-    local bail = function(sig)
-      handle:rmwatch(wd)
-      handle:close()
-      cfg.LOG(opts.syslog, opts.log, string.format("Caught signal %s. Exiting.", core.tostring(sig)), syslog.LOG_ERR)
-      os.exit(255)
-    end
-    signal.signal(signal.SIGINT, bail)
-    signal.signal(signal.SIGTERM, bail)
-    handle:read()
-    collectgarbage()
-  elseif opts.periodic then
-    unistd.sleep(opts.periodic)
-    collectgarbage()
-    goto RUN
-  else
-    break
-  end
 end
 
