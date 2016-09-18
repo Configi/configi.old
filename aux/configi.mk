8be73be3aec4f4c0b1c68a5b999eba9fdf869b07
Eduardo Tongson <propolice@gmail.com>
2016-09-18 10:50:58 -0400

    tests: Add posix.libgen to the dependencies.

--- aux/configi.mk
+++ aux/configi.mk
@@ -1,4 +1,4 @@
-testDEPS:= cwtest.lua cimicida.lua crc32.lua lib.lua factid.lua px.a factidC.a posix.sys.stat.a posix.pwd.a posix.grp.a posix.unistd.a posix.errno.a posix.sys.wait.a posix.poll.a posix.fcntl.a posix.stdlib.a posix.syslog.a posix.dirent.a
+testDEPS:= cwtest.lua cimicida.lua crc32.lua lib.lua factid.lua px.a factidC.a posix.sys.stat.a posix.pwd.a posix.grp.a posix.unistd.a posix.errno.a posix.sys.wait.a posix.poll.a posix.fcntl.a posix.stdlib.a posix.syslog.a posix.dirent.a posix.libgen.a
 CLEAN+= clean_tests
 
 tests: $(EXE)
