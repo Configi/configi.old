d31b49d37342ce2ff7c36bc47157127c3e3c49c0
Eduardo Tongson <propolice@gmail.com>
2016-10-02 01:28:52 +0800

    Recent changes broke tests compilation, fix it.

--- configi.mk
+++ configi.mk
@@ -1,11 +1,13 @@
-testDEPS:= cwtest.lua cimicida.lua crc32.lua lib.lua factid.lua px.a factidC.a posix.sys.stat.a posix.pwd.a posix.grp.a posix.unistd.a posix.errno.a posix.sys.wait.a posix.poll.a posix.fcntl.a posix.stdlib.a posix.syslog.a posix.dirent.a posix.libgen.a
+testsDEPS:= cwtest.lua cimicida.lua crc32.lua lib.lua factid.lua
+testsDEPS_A:= px.a factidC.a posix.sys.stat.a posix.pwd.a posix.grp.a posix.unistd.a posix.errno.a posix.sys.wait.a posix.poll.a posix.fcntl.a posix.stdlib.a posix.syslog.a posix.dirent.a posix.libgen.a
 CLEAN+= clean_tests
 
-bin/tests: $(EXE)
-	$(CP) vendor/lua/cwtest.lua .
+bin/tests: $(EXE_T)
+	for f in $(testsDEPS); do $(CP) vendor/lua/$$f .;done
 	$(ECHOT) LN $@
-	CC=$(TARGET_STCC) NM=$(TARGET_NM) $(LUA_T) $(LUASTATIC) bin/tests.lua $(testDEPS) $(LUA_A) $(TARGET_FLAGS) $(PIE) $(TARGET_LDFLAGS) 2>&1 >/dev/null
-	$(RM) $(RMFLAGS) bin/tests.lua.c cwtest.lua cimicida.lua crc32.lua lib.lua factid.lua
+	CC=$(TARGET_STCC) NM=$(TARGET_NM) $(LUA_T) $(LUASTATIC) bin/tests.lua $(testsDEPS) $(testsDEPS_A) $(LUA_A) \
+		 $(TARGET_FLAGS) $(PIE) $(TARGET_LDFLAGS) 2>&1 >/dev/null
+	$(RM) $(RMFLAGS) bin/tests.lua.c $(testsDEPS)
 
 clean_tests:
 	$(RM) $(RMFLAGS) bin/tests
