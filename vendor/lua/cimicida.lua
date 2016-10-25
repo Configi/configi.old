72d813724aef3788be826a8d827663f8cb2d59b5
Eduardo Tongson <propolice@gmail.com>
2016-10-25 10:23:15 +0800

    Full name for Ada-case.

--- vendor/lua/cimicida.lua
+++ vendor/lua/cimicida.lua
@@ -740,8 +740,8 @@ end
 
 --- @export
 return {
-    pcall_f = pcall_f, Pcall_F = pcall_f,
-    try_f = try_f, Try_F = try_f,
+    pcall_f = pcall_f, Pcall_Factory = pcall_f,
+    try_f = try_f, Try_Factory = try_f,
     require = xrequire, Require = xrequire,
     printf = printf, Print = printf,
     fprintf = fprintf, Fprint = fprintf,
