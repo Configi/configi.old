87affa6c6701157edc3cc1ccebe912d7398ebff0
Eduardo Tongson <propolice@gmail.com>
2016-11-12 20:53:45 +0800

    Conform to BSD style.

--- vendor/c/factid/src/factidC.c
+++ vendor/c/factid/src/factidC.c
@@ -18,57 +18,33 @@
 #ifndef _LINUX_SYSINFO_H
 # include <sys/sysinfo.h>
 #endif
-#ifdef HAVE_UCLIBC
-# include <utmp.h>
-# define utmpx utmp
-# define utmxpname utmpname
-# define setutxent setutent
-# define getutxent getutent
-# define endutxent endutent
-# define getutxline getutline
-# define getutxid getutid
-# define pututxline pututline
-#else
-# include <utmpx.h>
-#endif
+#include <utmpx.h>
 
 #include <assert.h>
 
 #include "lua.h"
 #include "lauxlib.h"
 #include "lualib.h"
+#include "auxlib.h"
 
 #ifndef _UTSNAME_LENGTH
 #define _UTSNAME_LENGTH 65
 #define _UTSNAME_DOMAIN_LENGTH 65
 #endif
 
-static int pusherror(lua_State *L, char *error)
-{
-	lua_pushnil(L);
-	lua_pushstring(L, error);
-	return 2;
-}
-
-static int pusherrno(lua_State *L, char *error)
-{
-	lua_pushnil(L);
-	lua_pushfstring(L, LUA_QS" : "LUA_QS, error, strerror(errno));
-	lua_pushinteger(L, errno);
-	return 3;
-}
-
-
 /*
 Uptime via sysinfo(2).
 @function uptime
 @return uptime (NUMBER)
 */
-static int Fuptime(lua_State *L)
+static int
+Fuptime(lua_State *L)
 {
 	struct sysinfo info = {0};
 
-	if (sysinfo(&info) == -1) return pusherrno(L, "sysinfo(2) error");
+	if (sysinfo(&info) == -1) {
+		return luaX_pusherrno(L, "sysinfo(2) error");
+	}
 	lua_createtable(L, 0, 5);
 	lua_pushinteger(L, info.uptime);
 	lua_setfield(L, -2, "totalseconds");
@@ -90,12 +66,15 @@ Load averages via sysinfo(2)
 @function loads
 @return load averages (TABLE)
 */
-static int Floads(lua_State *L)
+static int
+Floads(lua_State *L)
 {
 	int l;
 	struct sysinfo info = {0};
 
-	if (sysinfo(&info) == -1) return pusherrno(L, "sysinfo(2) error");
+	if (sysinfo(&info) == -1) {
+		return luaX_pusherrno(L, "sysinfo(2) error");
+	}
 	lua_createtable(L, 3, 0);
 	for (l = 0; l < 3; l++) {
 		lua_pushnumber(L, info.loads[l]/65536.00);
@@ -109,11 +88,14 @@ Memory usage via sysinfo(2)
 @function mem
 @return memory usage (TABLE)
 */
-static int Fmem(lua_State *L)
+static int
+Fmem(lua_State *L)
 {
 	struct sysinfo info = {0};
 
-	if (sysinfo(&info) == -1) return pusherrno(L, "sysinfo(2) error");
+	if (sysinfo(&info) == -1) {
+		return luaX_pusherrno(L, "sysinfo(2) error");
+	}
 	lua_createtable(L, 0, 9);
 	lua_pushinteger(L, info.mem_unit);
 	lua_setfield(L, -2, "mem_unit");
@@ -141,11 +123,14 @@ Number of processes via sysinfo(2)
 @function procs
 @return number of processes (NUMBER)
 */
-static int Fprocs(lua_State *L)
+static int
+Fprocs(lua_State *L)
 {
 	struct sysinfo info = {0};
 
-	if (sysinfo(&info) == -1) return pusherrno(L, "sysinfo(2) error");
+	if (sysinfo(&info) == -1) {
+		return luaX_pusherrno(L, "sysinfo(2) error");
+	}
 	lua_pushinteger(L, info.procs);
 	return 1;
 }
@@ -155,7 +140,8 @@ Show sysconf(3) information
 @function sysconf
 @return values (TABLE)
 */
-static int Fsysconf(lua_State *L)
+static int
+Fsysconf(lua_State *L)
 {
 	struct {
 		char *name;
@@ -184,15 +170,15 @@ Hostname via gethostname(2)
 @function hostname
 @return hostname (STRING)
 */
-static int Fhostname(lua_State *L)
+static int
+Fhostname(lua_State *L)
 {
 	char hostname[1026]; // NI_MAXHOST + 1
 	if (!gethostname(hostname, (sizeof hostname)-1)) {
 		hostname[(sizeof hostname)-1] = '\0';
-		assert(hostname[1025] == '\0');
 		lua_pushstring(L, hostname);
 	} else {
-		return pusherrno(L, "gethostname(2) error");
+		return luaX_pusherrno(L, "gethostname(2) error");
 	}
 	return 1;
 }
@@ -202,46 +188,37 @@ Show uname information via uname(2)
 @function uname
 @return information (TABLE)
 */
-static int Funame(lua_State *L)
+static int
+Funame(lua_State *L)
 {
 	struct utsname uts = {0};
-	char buf[_UTSNAME_LENGTH];
-	char dbuf[_UTSNAME_DOMAIN_LENGTH];
+	char buf[_UTSNAME_LENGTH] = {0};
+	char dbuf[_UTSNAME_DOMAIN_LENGTH] = {0};
 
-	if (uname(&uts) == -1) return pusherrno(L, "uname(2) error");
+	if (uname(&uts) == -1) {
+		return luaX_pusherrno(L, "uname(2) error");
+	}
 	lua_createtable(L, 0, 6);
-	memset(&buf, 0, _UTSNAME_LENGTH);
-	strncpy(buf, uts.sysname, _UTSNAME_LENGTH);
-	buf[_UTSNAME_LENGTH-1] = '\0';
+	strnmove(buf, uts.sysname, _UTSNAME_LENGTH);
 	lua_pushstring(L, buf);
 	lua_setfield(L, -2, "sysname");
-	memset(&buf, 0, _UTSNAME_LENGTH);
-	strncpy(buf, uts.nodename, _UTSNAME_LENGTH);
-	buf[_UTSNAME_LENGTH-1] = '\0';
+	strnmove(buf, uts.nodename, _UTSNAME_LENGTH);
 	lua_pushstring(L, buf);
 	lua_setfield(L, -2, "nodename");
-	memset(&buf, 0, _UTSNAME_LENGTH);
-	strncpy(buf, uts.release, _UTSNAME_LENGTH);
-	buf[_UTSNAME_LENGTH-1] = '\0';
+	strnmove(buf, uts.release, _UTSNAME_LENGTH);
 	lua_pushstring(L, buf);
 	lua_setfield(L, -2, "release");
-	memset(&buf, 0, _UTSNAME_LENGTH);
-	strncpy(buf, uts.version, _UTSNAME_LENGTH);
-	buf[_UTSNAME_LENGTH-1] = '\0';
+	strnmove(buf, uts.version, _UTSNAME_LENGTH);
 	lua_pushstring(L, buf);
 	lua_setfield(L, -2, "version");
-	memset(&buf, 0, _UTSNAME_LENGTH);
-	strncpy(buf, uts.machine, _UTSNAME_LENGTH);
-	buf[_UTSNAME_LENGTH-1] = '\0';
+	strnmove(buf, uts.machine, _UTSNAME_LENGTH);
 	lua_pushstring(L, buf);
 	lua_setfield(L, -2, "machine");
-	memset(&dbuf, 0, _UTSNAME_DOMAIN_LENGTH);
 #ifdef _GNU_SOURCE
-	strncpy(dbuf, uts.domainname, _UTSNAME_DOMAIN_LENGTH);
+	strnmove(dbuf, uts.domainname, _UTSNAME_DOMAIN_LENGTH);
 #else
-	strncpy(dbuf, uts.__domainname, _UTSNAME_DOMAIN_LENGTH);
+	strnmove(dbuf, uts.__domainname, _UTSNAME_DOMAIN_LENGTH);
 #endif
-	dbuf[_UTSNAME_DOMAIN_LENGTH-1] = '\0';
 	lua_pushstring(L, dbuf);
 	lua_setfield(L, -2, "domainname");
 	return 1;
@@ -252,12 +229,12 @@ Hostid via gethostid(3)
 @function hostid
 @return hostid (STRING)
 */
-static int Fhostid(lua_State *L)
+static int
+Fhostid(lua_State *L)
 {
-	char hostid[9];
+	char hostid[9] = {0};
 
 	snprintf(hostid, sizeof hostid, "%08lx", gethostid());
-	assert(hostid[8] == '\0');
 	lua_pushstring(L, hostid);
 	return 1;
 }
@@ -267,7 +244,8 @@ Current timezone via strftime(3)
 @function timezone
 @return timezone (STRING)
 */
-static int Ftimezone(lua_State *L)
+static int
+Ftimezone(lua_State *L)
 {
 	char tzbuf[4];
         struct tm *te = {0};
@@ -276,7 +254,9 @@ static int Ftimezone(lua_State *L)
         te = localtime(&t);
 
         setlocale(LC_TIME, "C");
-	if (!strftime(tzbuf, sizeof tzbuf, "%Z", te)) return pusherror(L, "strftime(3) error");
+	if (!strftime(tzbuf, sizeof tzbuf, "%Z", te)) {
+		return luaX_pusherror(L, "strftime(3) error");
+	}
 	assert(tzbuf[3] == '\0');
 	lua_pushstring(L, tzbuf);
 	return 1;
@@ -287,15 +267,22 @@ Mount information via getmntent(3)
 @function mount
 @return mount information (TABLE)
 */
-static int Fmount(lua_State *L)
+static int
+Fmount(lua_State *L)
 {
 	int c = 0;
 	struct mntent *m = {0};
 	FILE *mtab = setmntent("/etc/mtab", "r");
 
-	if (!mtab) mtab = setmntent("/proc/self/mounts", "r");
-	if (!mtab) return pusherrno(L, "setmntent(3) error");
-	if (setvbuf(mtab, (void *)0, _IONBF, 0)) return pusherrno(L, "setvbuf(3) error");
+	if (!mtab) {
+		mtab = setmntent("/proc/self/mounts", "r");
+	}
+	if (!mtab) {
+		return luaX_pusherrno(L, "setmntent(3) error");
+	}
+	if (setvbuf(mtab, (void *)0, _IONBF, 0)) {
+		return luaX_pusherrno(L, "setvbuf(3) error");
+	}
 	lua_newtable(L);
 	while ((m = getmntent(mtab))) {
 		lua_createtable(L, 0, 6);
@@ -323,7 +310,8 @@ Deduce outgoing IPv4 and IPv6 address
 @function ipaddress
 @return addresses (TABLE)
 */
-static int Fipaddress(lua_State *L)
+static int
+Fipaddress(lua_State *L)
 {
 	int fd4, fd6, c;
 	char ipv6[INET6_ADDRSTRLEN];
@@ -348,23 +336,35 @@ static int Fipaddress(lua_State *L)
 	inet_pton(AF_INET6, "2001:4860:4860::8888", &r6.sin6_addr);
 
 	lua_createtable(L, 0, 2);
-	if ((fd4 = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) return pusherrno(L, "socket(2) error");
+	if ((fd4 = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+		return luaX_pusherrno(L, "socket(2) error");
+	}
 	for (c = 0; c <= 3; c++) {
-		if (!connect(fd4, (struct sockaddr *)&r4, sizeof r4)) break;
+		if (!connect(fd4, (struct sockaddr *)&r4, sizeof r4)) {
+			break;
+		}
 		if (c == 3) {
 			inet_pton(AF_INET, "127.0.0.1", &r4.sin_addr.s_addr);
-			if (connect(fd4, (struct sockaddr *)&r4, sizeof r4) == -1) return pusherrno(L, "connect(2) error");
+			if (connect(fd4, (struct sockaddr *)&r4, sizeof r4) == -1) {
+				return luaX_pusherrno(L, "connect(2) error");
+			}
 		}
 	}
-	if (getsockname(fd4, (struct sockaddr *)&ip4, &ip4len) == -1) return pusherrno(L, "getsockname(2) error");
+	if (getsockname(fd4, (struct sockaddr *)&ip4, &ip4len) == -1) {
+		return luaX_pusherrno(L, "getsockname(2) error");
+	}
 	shutdown(fd4, 2);
 	inet_ntop(AF_INET, &ip4.sin_addr, ipv4, INET_ADDRSTRLEN);
 	lua_pushstring(L, ipv4);
 	lua_setfield(L, -2, "ipv4");
 
-	if ((fd6 = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP)) == -1) return pusherrno(L, "socket(2) error");
+	if ((fd6 = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+		return luaX_pusherrno(L, "socket(2) error");
+	}
 	for (c = 0; c <= 3; c++) {
-		if (!connect(fd6, (struct sockaddr *)&r6, sizeof r6)) break;
+		if (!connect(fd6, (struct sockaddr *)&r6, sizeof r6)) {
+			break;
+		}
 		if (c == 3) {
 			inet_pton(AF_INET6, "::1", &r6.sin6_addr);
 			if (connect(fd6, (struct sockaddr *)&r6, sizeof r6) == -1) {
@@ -375,7 +375,9 @@ static int Fipaddress(lua_State *L)
 			}
 		}
 	}
-	if (getsockname(fd6, (struct sockaddr *)&ip6, &ip6len) == -1) return pusherrno(L, "getsockname(2) error");
+	if (getsockname(fd6, (struct sockaddr *)&ip6, &ip6len) == -1) {
+		return luaX_pusherrno(L, "getsockname(2) error");
+	}
 	shutdown(fd6, 2);
 	inet_ntop(AF_INET6, &ip6.sin6_addr, ipv6, INET6_ADDRSTRLEN);
 	lua_pushstring(L, ipv6);
@@ -388,7 +390,8 @@ Network interface statistics
 @function ifstats
 @return stats (TABLE)
 */
-static int ifstats(lua_State *L, struct ifaddrs *i)
+static int
+ifstats(lua_State *L, struct ifaddrs *i)
 {
 	struct rtnl_link_stats *stats = i->ifa_data;
 	lua_createtable(L, 0, 10);
@@ -416,7 +419,8 @@ static int ifstats(lua_State *L, struct ifaddrs *i)
 	return 1;
 }
 
-static int ipaddr(lua_State *L, struct ifaddrs *i)
+static int
+ipaddr(lua_State *L, struct ifaddrs *i)
 {
 	char ipv4[INET_ADDRSTRLEN];
 	char ipv6[INET6_ADDRSTRLEN];
@@ -442,21 +446,30 @@ Show configured IP addresses.
 @function ifaddrs
 @return information (TABLE)
 */
-static int Fifaddrs(lua_State *L)
+static int
+Fifaddrs(lua_State *L)
 {
 	struct ifaddrs *ifaddr = {0};
 	struct ifaddrs *i = {0};
 	int c = 1;
 
-	if (getifaddrs(&ifaddr) == -1) return pusherrno(L, "getifaddrs(3) error");
+	if (getifaddrs(&ifaddr) == -1) {
+		return luaX_pusherrno(L, "getifaddrs(3) error");
+	}
 	lua_newtable(L);
 	for (i = ifaddr; i != 0 ; i = i->ifa_next) {
-		if (i->ifa_addr == 0) continue;
+		if (i->ifa_addr == 0) {
+			continue;
+		}
 		lua_newtable(L);
 		lua_pushstring(L, i->ifa_name);
 		lua_setfield(L, -2, "interface");
-		if (i->ifa_addr->sa_family == AF_INET || i->ifa_addr->sa_family == AF_INET6) ipaddr(L, i);
-		if (i->ifa_data && (i->ifa_addr->sa_family == AF_PACKET)) ifstats(L, i);
+		if (i->ifa_addr->sa_family == AF_INET || i->ifa_addr->sa_family == AF_INET6) {
+			ipaddr(L, i);
+		}
+		if (i->ifa_data && (i->ifa_addr->sa_family == AF_PACKET)) {
+			ifstats(L, i);
+		}
 		lua_rawseti(L, -2, c++);
 	}
 	freeifaddrs(ifaddr);
@@ -468,7 +481,8 @@ Show MAC addresses
 @function macaddrs
 @return addresses (TABLE)
 */
-static int Fmacaddrs(lua_State *L)
+static int
+Fmacaddrs(lua_State *L)
 {
 	char buf[8192];
 	char mac[18];
@@ -477,14 +491,20 @@ static int Fmacaddrs(lua_State *L)
  	struct ifconf ifc = {0};
 	struct ifreq *cifr = {0};
 
-	if ((fd = socket(PF_INET, SOCK_DGRAM, 0)) == -1) return pusherrno(L, "socket(2) error");
+	if ((fd = socket(PF_INET, SOCK_DGRAM, 0)) == -1) {
+		return luaX_pusherrno(L, "socket(2) error");
+	}
 	ifc.ifc_len = sizeof buf;
 	ifc.ifc_buf = buf;
-	if (ioctl(fd, SIOCGIFCONF, &ifc) == -1) return pusherrno(L, "ioctl(2) error");
+	if (ioctl(fd, SIOCGIFCONF, &ifc) == -1) {
+		return luaX_pusherrno(L, "ioctl(2) error");
+	}
 	lua_newtable(L);
-	for(c = 0; c < (ifc.ifc_len/sizeof (struct ifreq)); c++) {
+	for (c = 0; c < (ifc.ifc_len/sizeof (struct ifreq)); c++) {
 		cifr = &ifc.ifc_req[c];
-		if (ioctl(fd, SIOCGIFHWADDR, cifr) == -1) return pusherrno(L, "ioctl(2) error");
+		if (ioctl(fd, SIOCGIFHWADDR, cifr) == -1) {
+			return luaX_pusherrno(L, "ioctl(2) error");
+		}
 		snprintf(mac, sizeof mac, "%02x:%02x:%02x:%02x:%02x:%02x",
 			(unsigned char)cifr->ifr_hwaddr.sa_data[0],
 			(unsigned char)cifr->ifr_hwaddr.sa_data[1],
@@ -504,7 +524,8 @@ Show utmp information via getutxent(3)
 @function utmp
 @return information (TABLE)
 */
-static int Futmp(lua_State *L)
+static int
+Futmp(lua_State *L)
 {
 	struct utmpx *utx = {0};
 	setutxent();
@@ -529,7 +550,8 @@ static int Futmp(lua_State *L)
 	return 1;
 }
 
-static const luaL_Reg F[] =
+static const
+luaL_Reg F[] =
 {
 	{"uptime", Fuptime},
 	{"loads", Floads},
@@ -548,7 +570,8 @@ static const luaL_Reg F[] =
 	{NULL, NULL}
 };
 
-LUALIB_API int luaopen_factidC(lua_State *L)
+int
+luaopen_factidC(lua_State *L)
 {
 	luaL_newlib(L, F);
 	return 1;
