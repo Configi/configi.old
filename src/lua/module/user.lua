3068776c04c67f4feb0912d1e996b0cff958a8bb
Eduardo Tongson <propolice@gmail.com>
2016-11-16 09:57:55 +0800

    G table was renamed a while ago.

--- src/lua/module/user.lua
+++ src/lua/module/user.lua
@@ -57,28 +57,28 @@ function user.present(S)
         elseif user then
             if P.shell and user.pw_shell ~= P.shell then
                 if F.run(cmd.usermod, { "-s", P.shell, P.login}) then
-                    F.msg(P.login, G.mod_shell, true, 0, string.format("From: %s To: %s", user.shell, P.shell))
+                    F.msg(P.login, M.report.mod_shell, true, 0, string.format("From: %s To: %s", user.shell, P.shell))
                     R.notify = P.notify
                     R.repaired = true
                 end
             end
             if P.uid and tostring(user.pw_uid) ~= P.uid then
                 if F.run(cmd.usermod, { "-u", P.uid, P.login}) then
-                    F.msg(P.login, G.mod_uid, true, 0, string.format("From: %s To: %s", user.uid, P.uid))
+                    F.msg(P.login, M.report.mod_uid, true, 0, string.format("From: %s To: %s", user.uid, P.uid))
                     R.notify = P.notify
                     R.repaired = true
                 end
             end
             if P.gid and tostring(user.pw_gid) ~= P.gid then
                 if F.run(cmd.usermod, { "-g", P.gid, P.login }) then
-                    F.msg(P.login, G.mod_gid, true, 0, string.format("From: %s To: %s", user.gid, P.gid))
+                    F.msg(P.login, M.report.mod_gid, true, 0, string.format("From: %s To: %s", user.gid, P.gid))
                     R.notify = P.notify
                     R.repaired = true
                 end
             end
             if P.home and user.dir ~= P.home then
                 if F.run(cmd.usermod, { "-m", "-d", P.home, P.login}) then
-                    F.msg(P.login, G.mod_home, true, 0, string.format("From: %s To: %s", user.dir, P.home))
+                    F.msg(P.login, M.report.mod_home, true, 0, string.format("From: %s To: %s", user.dir, P.home))
                     R.notify = P.notify
                     R.repaired = true
                 end
