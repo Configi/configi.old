e6db982f858d4f2322f29ee550bb6a60e5ae5479
Eduardo Tongson <propolice@gmail.com>
2016-11-16 22:00:39 +0800

    Convert embedded policy sample to Lua.

--- src/lua/policy.lua
+++ src/lua/policy.lua
@@ -1,10 +1,11 @@
-local policy = { moon = {} }
-policy.moon.init = [[
-include"include.moon"
-file.touch"test/tmp/core-embedded.txt"
-    mode: "0777"
+local policy = { lua = {} }
+policy.lua.init = [[
+include"include.lua"
+file.touch"test/tmp/core-embedded.txt" {
+    mode = "0777"
+}
 ]]
-policy.moon.include = [[
-file.touch"test/tmp/core-include.txt"!
+policy.lua.include = [[
+file.touch"test/tmp/core-include.txt"()
 ]]
 return policy
