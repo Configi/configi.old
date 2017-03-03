cee932ec3b8edf9c9d3842a91a500688554c1d73
Eduardo Tongson <propolice@gmail.com>
2017-03-04 00:55:07 +0800

    Update to omnia 0.8.0.

---
+++ lib/rules.mk
@@ -0,0 +1,73 @@
+$(LUAC_T):
+	$(ECHOT) CC $@
+	$(HOST_CC) -o $@ -DMAKE_LUAC $(FLAGS) $(ONE).c -lm
+
+$(LUA_T):
+	$(ECHOT) CC $@
+	$(HOST_CC) -o $@ -DMAKE_LUA -DLUA_USE_DLOPEN $(luaDEFINES) $(FLAGS) $(ONE).c -lm $(LUAT_FLAGS)
+
+$(HOST_LUA_O):
+	$(ECHOT) CC $@
+	$(HOST_CC) -o $@ -c -Ilib -DMAKE_LIB $(luaDEFINES) -fPIC $(FLAGS) $(ONE).c
+
+$(HOST_LUA_A): $(HOST_LUA_O)
+	$(ECHOT) AR $@
+	$(AR) $(ARFLAGS) $@ $< >/dev/null 2>&1
+	$(RANLIB) $@
+
+$(LUA_O):
+	$(ECHOT) CC $@
+	$(TARGET_DYNCC) -o $@ -c -Ilib -DMAKE_LIB $(luaDEFINES) $(TARGET_FLAGS) $(ONE).c
+
+$(LUA_A): $(LUA_O)
+	$(ECHOT) AR $@
+	$(TARGET_AR) $(ARFLAGS) $@ $< >/dev/null 2>&1
+	$(TARGET_RANLIB) $@
+
+$(VENDOR_TOP):
+	$(ECHOT) CP VENDOR
+	for f in $(VENDOR); do $(CP) $(VENDOR_P)/$$f.lua .; done
+
+$(SRC_TOP):
+	$(ECHOT) CP SRC
+	for f in $(SRC); do $(CP) $(SRC_P)/$$f.lua .; done
+
+$(SRC_LUA):
+	$(ECHOT) CP SRC_DIR
+	for d in $(SRC_DIRS); do [ -d $$d ] || $(CPR) $(SRC_P)/$$d .; done
+
+$(VENDOR_LUA):
+	$(ECHOT) CP VENDOR_DIR
+	for d in $(VENDOR_DIRS); do [ -d $$d ] || $(CPR) $(VENDOR_P)/$$d .; done
+
+$(EXE_T): $(BUILD_DEPS) $(LIBLUA_A) $(LUA_T) $(C_MODULES) $(COMPILED) $(VENDOR_TOP) $(SRC_TOP) $(SRC_LUA) $(VENDOR_LUA)
+	$(ECHOT) LN $(EXE_T)
+	CC=$(TARGET_STCC) NM=$(TARGET_NM) $(LUA_T) $(LUASTATIC) $(MAIN) \
+	   $(SRC_LUA) $(VENDOR_LUA) $(VENDOR_TOP) $(SRC_TOP) $(C_MODULES) $(LIBLUA_A) \
+	   $(TARGET_FLAGS) $(PIE) $(TARGET_LDFLAGS) 2>&1 >/dev/null
+	$(RM) $(RMFLAGS) $(MAIN).c $(VENDOR_TOP) $(SRC_TOP)
+	$(RMRF) $(VENDOR_DIRS) $(SRC_DIRS)
+
+dev: $(LUA_T) $(C_SHARED) $(COMPILED) $(VENDOR_TOP) $(SRC_TOP) $(SRC_LUA) $(VENDOR_LUA)
+	$(CPR) lib/luacheck .
+	bin/lua bin/luacheck.lua $(SRC_TOP) $(COMPILED) $(SRC_LUA) --exclude-files 'vendor/lua/*'
+
+clean: $(CLEAN)
+	$(ECHO) "Cleaning up..."
+	$(RM) $(RMFLAGS) $(MAIN).c $(LUA_O) $(LUA_T) $(LUAC_T) $(LUA_A) $(EXE_T) \
+	   $(HOST_LUA_A) $(HOST_LUA_O) $(COMPILED) $(VENDOR_TOP) $(SRC_TOP)
+	$(RMRF) $(SRC_DIRS) $(VENDOR_DIRS) luacheck
+	$(RMRF) *.a bin/*.dSYM
+	$(ECHO) "Done!"
+
+install: $(EXE_T)
+	$(ECHO) "Installing..."
+	$(INSTALL) -d $(PREFIX)/bin
+	$(INSTALL) -c $(EXE_T) $(PREFIX)/$(EXE_T)
+	$(ECHO) "Done!"
+
+new:
+	$(RMRF) vendor/lua/* vendor/c/* src/lua/* src/c/* \
+		bin/test.moon bin/moonc.lua bin/moonpick.lua bin/moor.moon Makefile
+	$(CP) lib/Makefile.pristine Makefile
+
---
+++ lib/tests.mk
@@ -0,0 +1,138 @@
+.POSIX:
+.SUFFIXES:
+NULSTRING:=
+CONFIGURE_P:= lib/configure
+INCLUDES_P:= -Ilib/lua
+ifeq ($(CROSS),)
+  CROSS:= $(NULSTRING)
+endif
+ifeq ($(CROSS_CC),)
+  CROSS_CC:= $(HOST_CC)
+endif
+LD= ld
+NM= nm
+AR= ar
+RANLIB= ranlib
+STRIP= strip
+CC= $(CROSS_CC)
+TARGET_DYNCC:= $(CROSS)$(CC) -fPIC
+TARGET_STCC:= $(CROSS)$(CC)
+TARGET_LD= $(CROSS)$(LD)
+TARGET_RANLIB= $(CROSS)$(RANLIB)
+TARGET_AR= $(CROSS)$(AR)
+TARGET_NM= $(CROSS)$(NM)
+TARGET_STRIP= $(CROSS)$(STRIP)
+
+# FLAGS when compiling for an OpenWRT target.
+ifneq (,$(findstring openwrt,$(TARGET_STCC)))
+  TARGET_CCOPT:= -Os -fomit-frame-pointer -pipe
+  TARGET_LDFLAGS= -Wl,--gc-sections -Wl,--strip-all
+endif
+
+# Append -static-libgcc to CFLAGS if GCC is detected.
+IS_CC:= $(shell $(CONFIGURE_P)/test-cc.sh $(TARGET_STCC))
+ifeq ($(IS_CC), GCC)
+  TARGET_CFLAGS+= -static-libgcc
+endif
+
+# Replace --gc-sections with -dead-strip on Mac
+IS_APPLE:= $(shell $(CONFIGURE_P)/test-mac.sh $(TARGET_STCC))
+ifeq ($(IS_APPLE), APPLE)
+  LDFLAGS:= -Wl,-dead_strip
+  TARGET_LDFLAGS:= -Wl,-dead_strip
+  TARGET_DYNCC+= -undefined dynamic_lookup
+else
+  LUAT_FLAGS:= -ldl -Wl,-E
+endif
+
+# luaposix needs to link to lrt < glibc 2.17
+FOUND_RT:= $(shell $(CONFIGURE_P)/test-lrt.sh $(TARGET_STCC))
+ifeq ($(FOUND_RT), 0)
+  CFLAGS_LRT= -lrt
+endif
+
+# Test for GCC LTO capability.
+ifeq ($(shell $(CONFIGURE_P)/test-gcc47.sh $(TARGET_STCC)), true)
+  ifeq ($(shell $(CONFIGURE_P)/test-binutils-plugins.sh $(CROSS)gcc-ar), true)
+    TARGET_CFLAGS+= -fwhole-program -flto -fuse-linker-plugin
+    TARGET_LDFLAGS+= -fwhole-program -flto
+    TARGET_RANLIB:= $(CROSS)gcc-ranlib
+    TARGET_AR:= $(CROSS)gcc-ar
+    TARGET_NM:= $(CROSS)gcc-nm
+  endif
+endif
+
+
+
+ifeq ($(filter posix,$(VENDOR_C)), posix)
+  HAVE_LINUX_NETLINK_H:= $(shell $(CONFIGURE_P)/test-netlinkh.sh $(TARGET_STCC))
+  HAVE_POSIX_FADVISE:= $(shell $(CONFIGURE_P)/test-posix_fadvise.sh $(TARGET_STCC))
+  HAVE_STRLCPY:= $(shell $(CONFIGURE_P)/test-strlcpy.sh $(TARGET_STCC))
+endif
+ifeq ($(filter px,$(VENDOR_C)), px)
+  HAVE_FCNTL_CLOSEM:= $(shell $(CONFIGURE_P)/test-F_CLOSEM.sh $(TARGET_STCC))
+endif
+ifeq ($(filter inotify,$(VENDOR_C)), inotify)
+  HAVE_SYS_INOTIFY_H:= $(shell $(CONFIGURE_P)/test-inotifyh.sh $(TARGET_STCC))
+endif
+
+### Lua Module specific defines and tests ####
+
+## linotify
+
+ifeq ($(filter linotify,$(VENDOR_C)), linotify)
+	ifneq ($(HAVE_SYS_INOTIFY_H), true)
+    $(error Linotify module requested but Inotify header \(sys/inotify.h\) missing!)
+  endif
+endif
+
+## luaposix
+
+ifeq ($(HAVE_LINUX_NETLINK_H), true)
+  luaposixDEFINES+= -DHAVE_LINUX_NETLINK_H
+endif
+ifeq ($(HAVE_POSIX_FADVISE), true)
+  luaposixDEFINES+= -DHAVE_POSIX_FADVISE
+endif
+ifeq ($(HAVE_STRLCPY), true)
+  luaposixDEFINES+= -DHAVE_STRLCPY
+endif
+
+## lpeg
+ifeq ($(DEBUG), 1)
+  lpegDEFINES= -DLPEG_DEBUG
+endif
+
+## px
+ifeq ($(HAVE_FCNTL_CLOSEM), true)
+  pxDEFINES+= -DHAVE_FCNTL_CLOSEM
+endif
+
+ifneq ($(MAKECMDGOALS), release)
+  CCWARN:= -Wall -Wextra -Wdeclaration-after-statement -Wredundant-decls -Wshadow -Wpointer-arith
+  TARGET_CFLAGS:= -O1 -fno-omit-frame-pointer -g
+  CFLAGS:= -O1 -fno-omit-frame-pointer -g
+  ifeq ($(shell $(CONFIGURE_P)/test-gcc48.sh $(CC)), true)
+	CFLAGS+= -fsanitize=address
+  endif
+  TARGET_CCOPT:= $(NULSTRING)
+  CCOPT:= $(NULSTRING)
+  TARGET_LDFLAGS:= $(NULSTRING)
+  LDFLAGS:= $(NULSTRING)
+else
+  DEFINES+= -DNDEBUG
+endif
+
+ifeq ($(STATIC), 1)
+  PIE:= $(NULSTRING)
+  TARGET_LDFLAGS+= -static
+else
+  ifneq ($(IS_CC), CLANG)
+    PIE:= -fPIE -pie
+  else
+    PIE:= -fPIE -Wl,-pie
+  endif
+endif
+
+TARGET_FLAGS:= $(DEFINES) $(INCLUDES_P) $(TARGET_CFLAGS) $(TARGET_CCOPT) $(CCWARN) $(CFLAGS_LRT)
+FLAGS:= $(DEFINES) $(INCLUDES_P) $(CFLAGS) $(CCOPT) $(CCWARN)
