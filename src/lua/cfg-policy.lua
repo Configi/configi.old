925f02c0a24e491fdad0c4dba4dd7eb9690d20b9
Eduardo Tongson <propolice@gmail.com>
2017-03-26 12:56:26 +0800

    Embedded policy with support for the new directory structure.

--- src/lua/cfg-policy.lua
+++ src/lua/cfg-policy.lua
@@ -1,11 +1,25 @@
-local policy = { lua = {} }
-policy.lua.init = [[
+local policy = {["."]={},attributes={},policies={},handlers={}}
+policy.attributes.test = [[
+ file="test"
+]]
+policy.policies.test = [[
+file.touch"test/tmp/core-embedded-structure"{
+    comment = _"{{file}} policies",
+    notify = "embedded"
+}
+]]
+policy.handlers.test = [[
+file.touch"test/tmp/core-embedded-handlers"{
+    comment = _"{{file}} handlers",
+    handle = "embedded"
+]]
+policy["."]["init"] = [[
 include"include.lua"
 file.touch"test/tmp/core-embedded.txt" {
     mode = "0777"
 }
 ]]
-policy.lua.include = [[
+policy["."]["include"] = [[
 file.touch"test/tmp/core-include.txt"()
 ]]
 return policy
