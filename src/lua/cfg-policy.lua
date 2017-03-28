48fbd0b3f8bac00af981c1a755ba29e9c57b0867
Eduardo Tongson <propolice@gmail.com>
2017-03-28 09:57:45 +0800

    We were overwriting file. Missing closing bracket.

--- src/lua/cfg-policy.lua
+++ src/lua/cfg-policy.lua
@@ -1,17 +1,18 @@
 local policy = {["."]={},attributes={},policies={},handlers={}}
 policy.attributes.test = [[
- file="test"
+    var="test"
 ]]
 policy.policies.test = [[
 file.touch"test/tmp/core-embedded-structure"{
-    comment = _"{{file}} policies",
+    comment = _"{{var}} policies",
     notify = "embedded"
 }
 ]]
 policy.handlers.test = [[
 file.touch"test/tmp/core-embedded-handlers"{
-    comment = _"{{file}} handlers",
+    comment = _"{{var}} handlers",
     handle = "embedded"
+}
 ]]
 policy["."]["init"] = [[
 include"include.lua"
