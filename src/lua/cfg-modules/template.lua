f98a4298f5423786b40338f109a1baece4ef13ee
Eduardo Tongson <propolice@gmail.com>
2017-04-16 18:28:12 +0800

    Documentation change from MoonScript to plain Lua.

--- src/lua/cfg-modules/template.lua
+++ src/lua/cfg-modules/template.lua
@@ -52,12 +52,13 @@ end
 -- @param src source template [REQUIRED] [ALIAS: template]
 -- @param table [REQUIRED] [ALIAS: view]
 -- @param lua [ALIAS: data]
--- @param mode mode bits for output file [DEFAULT: "0600"]
--- @param diff show diff [CHOICES: "yes","no"]
--- @usage template.render("/etc/something/config")
---     template: "/etc/something/config.template"
---     view: "view_model"
---     data: "/etc/something/config.lua"
+-- @param mode mode bits for output file [DEFAULT: 0600]
+-- @param diff show diff [DEFAULT: false]
+-- @usage template.render("/etc/something/config"){
+--     template = "etc/something/config.template",
+--         view = "view_model",
+--         data = "/etc/something/config.lua"
+-- }
 function template.render(S)
     M.parameters = { "src", "lua", "table", "mode", "diff" }
     M.report = {
