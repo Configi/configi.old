57427e209220a013516e1a9145301c211f4e187a
Eduardo Tongson <propolice@gmail.com>
2017-06-24 15:59:53 +0800

    Full exec should be default.

--- bin/tests/ENV.lua
+++ bin/tests/ENV.lua
@@ -9,7 +9,7 @@ E.string = string
 E.stat = require "posix.sys.stat"
 E.lib = require "lib"
 E.cmd = E.lib.exec.cmd
-E.cfg = E.cmd["-bin/cfg-agent.lua"]
+E.cfg = E.cmd["bin/cfg-agent.lua"]
 E.file = E.lib.file
 E.dir = "test/tmp/"
 return E
