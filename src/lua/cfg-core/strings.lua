a351ab74d584dc543f5bb253ad1d8c16ee8fda28
Eduardo Tongson <propolice@gmail.com>
2017-06-27 10:32:31 +0800

    Style, indention, spacing updates.

--- src/lua/cfg-core/strings.lua
+++ src/lua/cfg-core/strings.lua
@@ -1,47 +1,48 @@
 return {
-    version = "Configi 2.0.0",
-    short_args = "hvtdsmjVxl:p:g:r:f:e:",
-    long_args = {
-        {"help", "none", "h"},
-        {"debug", "none", "v"},
-        {"test", "none", "t"},
-        {"dump", "none", "d"},
-        {"watch", "none", "w"},
-        {"periodic", "none", "p"},
-        {"syslog", "none", "s"},
-        {"log", "required", "l"},
-        {"msg", "none", "m"},
-        {"version", "none", "V"},
-        {"tag", "required", "g"},
-        {"runs", "required", "r"},
-        {"file", "required", "f"},
-        {"embedded","required", "e"},
-        {"exec", "none", "x"},
-    },
-    help = [[
-    cfg [-h] [-V] [-v] [-d] [-t] [-w] [-p N] [-s] [-l FILE] [-m] [-g TAG] [-r N] [-f "POLICY"] [-e "POLICY"]
+  version = "Configi 2.0.0",
+  short_args = "hvtdsmjVxl:p:g:r:f:e:",
+  long_args = {
+    {"help", "none", "h"},
+    {"debug", "none", "v"},
+    {"test", "none", "t"},
+    {"dump", "none", "d"},
+    {"watch", "none", "w"},
+    {"periodic", "none", "p"},
+    {"syslog", "none", "s"},
+    {"log", "required", "l"},
+    {"msg", "none", "m"},
+    {"version", "none", "V"},
+    {"tag", "required", "g"},
+    {"runs", "required", "r"},
+    {"file", "required", "f"},
+    {"embedded","required", "e"},
+    {"exec", "none", "x"},
+  },
+  help = [[
+  cfg-agent [-h] [-V] [-d] [-t] [-w] [-p N] [-s] [-m] [-x] [-v] [-l FILE] [-g TAG] [-r N] [-f POLICY] [-e POLICY]
 
-        Options:
-            -h, --help                  This help text.
-            -V, --version               Print version.
-            -v, --debug                 Turn on debugging messages.
-            -d, --dump                  Dump tree.
-            -t, --test                  Dry-run mode. All operations are expected to succeed. Turns on debugging.
-            -w, --watch                 Watch for inotify IN_MODIFY and IN_ATTRIB events to the main policy file.
-            -p, --periodic              Do a run after N seconds.
-            -s, --syslog                Enable logging to syslog.
-            -l, --log                   Log to an specified file.
-            -m, --msg                   Show debug and test messages.
-            -g, --tag                   Only run specified tag(s).
-            -r, --runs                  Run the policy N times if a failure is encountered. Default is 3.
-            -f, --file                  Path to the Configi policy.
-            -e, --embedded              Name of the embedded Configi policy.
+      Options:
+          -h  This help text.
+          -V  Print version.
+          -d  Dump tree.
+          -t  Dry-run mode. All operations are expected to succeed. Turns on full debugging (-v).
+          -w  Watch for inotify IN_MODIFY and IN_ATTRIB events to the main policy file.
+          -p  Do a run after N seconds.
+          -s  Enable logging to syslog.
+          -l  Log to an specified FILE.
+          -m  Show minimal debug and test messages.
+          -x  Show execution debug messages.
+          -v  Show full debugging messages.
+          -g  Only run specified TAG(s).
+          -r  Run the policy N times if a failure is encountered. Default is 3.
+          -f  Path to the Configi POLICY.
+          -e  Name of the embedded Configi POLICY.
 
 ]],
-    IDENT = "Configi",
-    ERROR = "ERROR: ",
-     WARN = "WARNING: ",
-     SERR = "POLICY ERROR: ",
-     MERR = "MODULE ERROR: ",
-     EXEC = "EXECUTION: "
+  IDENT = "Configi",
+  ERROR = "ERROR: ",
+  WARN = "WARNING: ",
+  SERR = "POLICY ERROR: ",
+  MERR = "MODULE ERROR: ",
+  EXEC = "EXECUTION: "
 }
