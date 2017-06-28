195fabeb61b7c5c6132f941a5ddcae77005b9430
Eduardo Tongson <propolice@gmail.com>
2017-06-28 15:56:16 +0800

    More functions for tests.

--- bin/tests/ENV.lua
+++ bin/tests/ENV.lua
@@ -3,13 +3,17 @@ E.T = require "u-test"
 E.factid = require "factid"
 E.dofile = dofile
 E.ipairs = ipairs
+E.pairs = pairs
+E.require = require
 E.os = os
 E.table = table
 E.string = string
 E.stat = require "posix.sys.stat"
 E.lib = require "lib"
+E.crc32 = require "plc.checksum".crc32
 E.cmd = E.lib.exec.cmd
 E.cfg = E.cmd["bin/cfg-agent.lua"]
 E.file = E.lib.file
+E.path = E.lib.path
 E.dir = "test/tmp/"
 return E
