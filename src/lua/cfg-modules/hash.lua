60e7a5f52e9f321492baf86cefc5649aca831cd2
Eduardo Tongson <propolice@gmail.com>
2017-06-29 14:02:19 +0800

    sha256 module renamed to hash.

--- src/lua/cfg-modules/hash.lua
+++ src/lua/cfg-modules/hash.lua
@@ -1,49 +1,46 @@
---- Verify the SHA256 hash of a file.
--- @module sha256
+--- Verify the hash of a file
+-- @module hash
 -- @author Eduardo Tongson <propolice@gmail.com>
 -- @license MIT <http://opensource.org/licenses/MIT>
--- @added 0.9.6
+-- @added 2.0.0
 
-local ENV, M, sha256 = {}, {}, {}
-local io = io
-local cfg = require"cfg-core.lib"
-local sha2 = require"sha2"
-local stat = require"posix.sys.stat"
-_ENV = ENV
+local M, hash = {}, {}
+local string = string
+local file = require "lib".file
+local cfg = require "cfg-core.lib"
+local bin = require "plc.bin"
+local sha2 = require "plc.sha2"
+local stat = require "posix.sys.stat"
+_ENV = nil
 
 M.required = { "path", "hash" }
 M.alias = {}
 M.alias.hash = { "digest", "signature" }
 
---- Check that a given hash matches the actual SHA256 hash of a file.
+--- Check that a given hash value matches the actual hash value of a file.
 -- Useful for alerting on changed hashes.
 -- @Promiser path of file to hash
--- @Aliases check
--- @param hash the 32-byte alphanumeric string to match for [ALIAS: digest, signature] [REQUIRED]
--- @usage sha256.verify("/etc/passwd"){
+-- @Aliases sha256
+-- @param hash the alphanumeric string to match for [ALIAS: digest, signature] [REQUIRED]
+-- @usage hash.sha2("/etc/passwd"){
 --     hash = "09e40b7b232c4abb427f1344e636e44ebf5684f70fb6cd67507e88955064255d"
 -- }
-function sha256.verify(S)
+function hash.sha2(S)
   M.report = {
-    repaired = "sha256.verify: Hash matched.",
-      kept = "sha256.verify: Hash matched.",
-      failed = "sha256.verify: Hash mismatch.",
-     missing = "sha256.verify: Missing path."
+    repaired = "hash.sha2: Digests matched.",
+    kept = "hash.sha2: Digests matched.",
+    failed = "hash.sha2: Mismatched digests.",
+    missing = "hash.sha2: Missing path."
   }
   return function(P)
     P.path = S
     local F, R = cfg.init(P, M)
-    if R.kept then
-      return F.kept(P.path)
-    end
+    P.hash = string.lower(P.hash)
+    if R.kept then return F.kept(P.path) end
     if not stat.stat(P.path) then
       return F.result(P.path, nil, M.report.missing)
     end
-    local csum = sha2.new256()
-    for f in io.lines(P.path, 2^12) do
-      csum:add(f)
-    end
-    if csum:close() ==  P.hash then
+    if P.hash == bin.stohex(sha2.hash256(file.read_to_string(P.path))) then
       return F.kept(P.path)
     else
       return F.result(P.path)
@@ -51,5 +48,5 @@ function sha256.verify(S)
   end
 end
 
-sha256.check = sha256.verify
-return sha256
+hash.sha256 = hash.sha2
+return hash
