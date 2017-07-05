098df34fca405796565722dc3c61bdfdfa8bea9c
Eduardo Tongson <propolice@gmail.com>
2017-07-05 09:54:33 +0800

    Login executable in Gentoo is in /bin/

--- test/process_running.lua
+++ test/process_running.lua
@@ -3,6 +3,14 @@ shell.command"/bin/touch /tmp/RUNNING"{
 }
 process.running"/usr/bin/login"{
     cmdline = "login -- root",
+    context = fact.osfamily.rhel,
+    name = "login",
+    notify_kept = "running"
+}
+
+process.running"/bin/login"{
+    cmdline = "/bin/login --",
+    context = fact.osfamily.gentoo,
     name = "login",
     notify_kept = "running"
 }
