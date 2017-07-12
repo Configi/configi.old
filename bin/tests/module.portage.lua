2c606a40f5b49c09c486e55a70e94f0f6ae1f170
Eduardo Tongson <propolice@gmail.com>
2017-07-12 09:34:15 +0800

    Converted module test.

---
+++ bin/tests/module.portage.lua
@@ -0,0 +1,23 @@
+_ENV = require "bin/tests/ENV"
+local osfamily = require"factid".osfamily()
+if not osfamily == "gentoo" then
+  T.emerge.skip = true
+end
+function present(p)
+  T.portage["present policy"] = function()
+    T.equal(cfg("-f", p), 0)
+  end
+  T.portage["present check"] = function()
+    T.is_not_nil(stat.stat("/usr/bin/spc"))
+  end
+end
+present("test/portage_present.lua")
+function absent(p)
+  T.portage["absent policy"] = function()
+    T.equal(cfg("-f", p), 0)
+  end
+  T.portage["absent check"] = function()
+    T.is_nil(stat.stat("/usr/bin/spc"))
+  end
+end
+absent("test/portage_absent.lua")
