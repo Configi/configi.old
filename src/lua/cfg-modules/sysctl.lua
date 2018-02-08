a420b4a59fd98b2390781240b60032bcad4afa52
Eduardo Tongson <propolice@gmail.com>
2018-02-08 17:03:24 +0800

    Use new function that discards EOL so we don't need to hack around it.

--- src/lua/cfg-modules/sysctl.lua
+++ src/lua/cfg-modules/sysctl.lua
@@ -80,7 +80,7 @@ function sysctl.read(S)
     if not file.stat(key) then
       return F.result(P.key, nil, M.report.not_found)
     end
-    if file.read(key) ~= tostring(P.value.."\n") then
+    if file.read_line(key) ~= tostring(P.value) then
       return F.result(P.key)
     end
     return F.result(P.key, false)
