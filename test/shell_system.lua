659d34e1be84c7c3299df80c99976c0f3da77700
Eduardo Tongson <propolice@gmail.com>
2018-02-08 15:28:02 +0800

    There is shell.script() for scripts now.

--- test/shell_system.lua
+++ test/shell_system.lua
@@ -1,3 +1,3 @@
-shell.system"test/shell_system.sh"{
+shell.system"touch test/tmp/shell_system.txt"{
   creates = "test/tmp/shell_system.txt"
 }
--- test/shell_system.sh
+++
@@ -1,4 +0,0 @@
-set -efu
-cd test/tmp
-touch shell_system.txt
-
