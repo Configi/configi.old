045a546dad66c8a2c770656539eb6a296232ea8b
Eduardo Tongson <propolice@gmail.com>
2018-06-11 03:19:42 -0700

    u-cfg: No need of the parameterized test cases so we avoid loading debug too.

--- vendor/lua/u-cfg.lua
+++ vendor/lua/u-cfg.lua
@@ -245,13 +245,7 @@ local function handle_new_test(suite, test_name, test_function)
         all_test_cases[suite_name] = {}
     end
     all_test_cases[suite_name][test_name] = test_function
-
-    local info = debug.getinfo(test_function)
-    if info.nparams == nil and
-            string.sub(test_name, #test_name - 1, #test_name) ~= "_p"
-            or info.nparams == 0 then
-        run_test(suite, test_name, test_function)
-    end
+    run_test(suite, test_name, test_function)
 end
 
 local function lookup_test_with_params(suite, test_name)
