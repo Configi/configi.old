5450f41baf1674ba8b2e0ce2955abd326c8d7834
Eduardo Tongson <propolice@gmail.com>
2018-07-16 18:29:24 -0700

    tests: Tests for exec.simple.

--- bin/cfg.lua
+++ bin/cfg.lua
@@ -2,11 +2,12 @@ local T = {
   "file_directory",
   "file_absent",
   "file_managed",
+  "exec_simple"
 }
 os.execute "mkdir tmp"
 local CFG = require "u-cfg"
 for _, t in ipairs(T) do
-local L = require("tests."..t)
+  require("tests."..t)
 end
 os.execute "rmdir tmp"
 CFG.summary()
