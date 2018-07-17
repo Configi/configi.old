a6fef9ad178ea14192aa48b559d5c00785cf81fa
Eduardo Tongson <propolice@gmail.com>
2018-07-16 23:00:33 -0700

    tests: No need for the leading spaces.

--- bin/cfg.lua
+++ bin/cfg.lua
@@ -10,33 +10,33 @@ end
 os.execute "mkdir tmp"
 U["file.directory"] = function()
   local a = "tmp/cfg_test__file_directory"
-  U["  - run"] = function()
+  U["- run"] = function()
     r, t = T"file.directory"
     U.equal(0, r)
   end
-  U["  - directory created"] = function()
+  U["- directory created"] = function()
     U.is_true(table.find(t.output, OK))
     U.equal(a, os.is_dir(a))
   end
-  U["  - if directory exists"] = function()
+  U["- if directory exists"] = function()
     r, t = T"file.directory"
     U.is_true(table.find(t.output, SKIP))
   end
-  U["  - tear down"] = function()
+  U["- tear down"] = function()
     U.equal(0, rmdir(a))
   end
 end
 U["file.absent"] = function()
   local a = "tmp/cfg_test__file_absent"
-  U["  - run"] = function()
+  U["- run"] = function()
     r, t = T"file.absent"
     U.equal(0, r)
   end
-  U["  - file already absent"] = function()
+  U["- file already absent"] = function()
     U.is_true(table.find(t.output, SKIP))
     U.equal(nil, os.is_file(a))
   end
-  U["  - removing file"] = function()
+  U["- removing file"] = function()
     os.execute("touch " .. a)
     r, t = T"file.absent"
     U.equal(0, r)
