fc61dcca2d086e8d31665ee8c40b9f9fabab0c09
Eduardo Tongson <propolice@gmail.com>
2018-07-19 02:28:47 -0700

    tests: Simplified now.

--- bin/tests/file.managed.lua
+++ bin/tests/file.managed.lua
@@ -1,15 +1,2 @@
-local U = require "u-test"
-local T = {}
 local file = require "cfg-modules.file"
-local lib = require "lib"
-local F = lib.file
-local os = lib.os
-_ENV = nil
-local f = "tmp/____configi_test"
-file.managed("____configi_test")
-U["file.managed"] = function()
-  U.equal(f, os.is_file(f))
-  U.equal(F.read_all(f), "Contents of the file \"tmp/____configi_test\"\n")
-end
-os.execute("rm -f tmp/____configi_test")
-return T
+file.managed("____configi_test_file_managed")
