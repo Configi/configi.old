d2993aaad68854f9c5213dc2bcb5b34eab9b6d75
Eduardo Tongson <propolice@gmail.com>
2018-07-19 02:29:16 -0700

    tests: Simplified now.

--- bin/tests/exec.simple.lua
+++ bin/tests/exec.simple.lua
@@ -1,15 +1,6 @@
-local U = require "u-test"
-local T = {}
 local exec = require "cfg-modules.exec"
-local os = require"lib".os
-_ENV = nil
 local f = "tmp/touch"
 exec.simple("/bin/touch"){
   args = f,
   expects = f
 }
-U["exec.simple"] = function()
-  U.equal(f, os.is_file(f))
-end
-os.execute("rm -f tmp/touch")
-return T
