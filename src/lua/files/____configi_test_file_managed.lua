60e4425cc173f97ff3a1fc220f47d932eb891107
Eduardo Tongson <propolice@gmail.com>
2018-12-25 10:25:37 +0000

    build: Mistakenly committed object files.

--- src/lua/files/____configi_test_file_managed.lua
+++
@@ -1,7 +0,0 @@
-local F = {}
-F["____configi_test_file_managed"] = {}
-F["____configi_test_file_managed"]["path"] = "tmp/____configi_test_file_managed"
-F["____configi_test_file_managed"]["contents"] =
-[==[Contents of the file "tmp/____configi_test_file_managed"
-]==]
-return F
--- src/lua/files/____configi_test_file_templated.lua
+++
@@ -1,9 +0,0 @@
-local F = {}
-F["____configi_test_file_templated"] = {}
-F["____configi_test_file_templated"]["path"] = "tmp/____configi_test_file_templated"
-F["____configi_test_file_templated"]["contents"] =
-[==[Contents of the file "tmp/____configi_test_file_templated"
-My name is ${name}
-Age is ${age}
-]==]
-return F
