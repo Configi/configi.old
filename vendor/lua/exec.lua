e89d5540c4dd375acec14c2c4a25d5fc17ed5128
Eduardo Tongson <propolice@gmail.com>
2019-01-07 10:16:50 +0000

    exec: Fork-Exec implementation.

--- vendor/lua/exec.lua
+++ vendor/lua/exec.lua
@@ -15,14 +15,24 @@ int dup2(int oldfd, int newfd);
 int setenv(const char*, const char*, int);
 int execvp(const char *file, char *const argv[]);
 int chdir(const char *);
+int pipe(int fd[2]);
+typedef long unsigned int size_t;
+typedef long signed int ssize_t;
+ssize_t read(int, void *, size_t);
+ssize_t write(int, const void *, size_t);
+int fcntl(int, int, ...);
 ]])
 local strerror = function(s)
   s = s or "error"
   return string.format("%s: %s\n", s, ffi.string(C.strerror(ffi.errno())))
 end
 local octal = function(n) return tonumber(n, 8) end
+local STDIN = 0
 local STDOUT = 1
 local STDERR = 2
+local F_GETFL = 0x03
+local F_SETFL = 0x04
+local O_NONBLOCK = 0x800
 local ERETRY = function(fn)
   return function(...)
     local r, e
@@ -49,7 +59,6 @@ local redirect = function(io_or_filename, dest_fd)
     C.dup2(STDERR, STDOUT)
   elseif (io_or_filename == io.stderr or io_or_filename == STDERR) and dest_fd ~= STDERR then
     C.dup2(STDOUT, STDERR)
-
     -- otherwise handle file-based redirection
   else
     local fd = C.open(io_or_filename, bit.bor(O_WRONLY, O_CREAT), bit.bor(S_IRUSR, S_IWUSR))
@@ -61,15 +70,39 @@ local redirect = function(io_or_filename, dest_fd)
   end
 end
 
-exec.spawn = function (exe, args, env, cwd, stdout_redirect, stderr_redirect)
+exec.spawn = function (exe, args, env, cwd, stdin_string, stdout_redirect, stderr_redirect)
   args = args or {}
-
+  local stdout_tbl = {}
+  local stderr_tbl = {}
+  local stdin = ffi.new("int[2]")
+  local stdout = ffi.new("int[2]")
+  local stderr = ffi.new("int[2]")
+  if C.pipe(stdin) == -1 then return nil, strerror("pipe(2) for STDIN failed") end
+  if C.pipe(stdout) == -1 then return nil, strerror("pipe(2) for STDOUT failed") end
+  if C.pipe(stderr) == -1 then return nil, strerror("pipe(2) for STDERR failed") end
   local pid = C.fork()
   if pid < 0 then
     return nil, strerror("fork(2) failed")
   elseif pid == 0 then -- child process
-    redirect(stdout_redirect, STDOUT)
-    redirect(stderr_redirect, STDERR)
+    C.close(stdin[1])
+    C.close(stdout[0])
+    C.close(stderr[0])
+    if stdin_string then
+      C.dup2(stdin[0], STDIN)
+    end
+    if stdout_redirect then
+      redirect(stdout_redirect, STDOUT)
+    else
+      C.dup2(stdout[1], STDOUT)
+    end
+    if stderr_redirect then
+      redirect(stderr_redirect, STDERR)
+    else
+      C.dup2(stderr[1], STDERR)
+    end
+    C.close(stdin[0])
+    C.close(stdout[1])
+    C.close(stderr[1])
     local string_array_t = ffi.typeof('const char *[?]')
     -- local char_p_k_p_t   = ffi.typeof('char *const*')
     -- args is 1-based Lua table, argv is 0-based C array
@@ -98,12 +131,61 @@ exec.spawn = function (exe, args, env, cwd, stdout_redirect, stderr_redirect)
     argv[0] = exe
     argv[#args + 1] = nil
     if C.execvp(exe, ffi.cast("char *const*", argv)) == -1 then
-      return nil, strerror("execvp(3) failed")
+      return nil, strerror("execvp(2) failed")
     end
     assert(nil, "assertion failed: exec.spawn (should be unreachable!)")
   else
+    if stdin_string then
+      local len = string.len(stdin_string)
+      local str = ffi.new("char[?]", len + 1)
+      ffi.copy(str, stdin_string, len)
+      C.write(stdin[1], str, len)
+      C.close(stdin[1])
+    else
+      C.close(stdin[1])
+    end
     if ERETRY(C.waitpid)(pid, nil, 0) == -1 then return nil, strerror("waitpid(2) failed") end
+    local output = function(i, o)
+      local buf = ffi.new("char[?]", 1)
+      local flags = C.fcntl(i, F_GETFL, 0)
+      flags = bit.bor(flags, O_NONBLOCK)
+      if C.fcntl(i, F_SETFL, ffi.new("int", flags)) == -1 then
+        return nil, strerror("fcntl(2) failed")
+      end
+      local n, s, c
+      while true do
+        n = C.read(i, buf, 1)
+        if n == 0 then
+          break
+        elseif n > 0 then
+          c = ffi.string(buf, 1)
+          if c ~= "\n" then
+            s = string.format("%s%s", s or "", c)
+          elseif ffi.errno() == C.EAGAIN then
+            o[#o+1] = s
+            break
+          else
+            o[#o+1] = s
+            s = nil
+          end
+        elseif ffi.errno() == C.EAGAIN then
+          o[#o+1] = s
+          break
+        else
+          return nil, strerror("read(2) failed")
+        end
+      end
+    end
+    output(stdout[0], stdout_tbl)
+    output(stderr[0], stderr_tbl)
+    C.close(stdin[0])
+    C.close(stdin[1])
+    C.close(stdout[0])
+    C.close(stdout[1])
+    C.close(stderr[0])
+    C.close(stderr[1])
   end
+    return pid, stdout_tbl, stderr_tbl
 end
 
 exec.context = function(exe)
@@ -119,7 +201,7 @@ exec.context = function(exe)
         args[#args+1] = k
       end
     end
-    return exec.spawn(exe, args, args.env, args.cwd, args.stdout, args.stderr)
+    return exec.spawn(exe, args, args.env, args.cwd, args.stdin, args.stdout, args.stderr)
   end})
 end
 exec.ctx = exec.context
@@ -136,7 +218,7 @@ exec.cmd = setmetatable({},
         else
           args = {...}
         end
-        return exec.spawn(exe, args, args.env, args.cwd, args.stdout, args.stderr)
+        return exec.spawn(exe, args, args.env, args.cwd, args.stdin, args.stdout, args.stderr)
       end
     end
   })
