b099e7979f4f96fa3ad490de40a73fb806ead540
Eduardo Tongson <propolice@gmail.com>
2019-01-08 10:05:13 +0000

    modules: Should be the first and only arg.

--- rerun/lib/configi.sh
+++ rerun/lib/configi.sh
@@ -1,15 +1,15 @@
 COMMAND="$1"
 REPAIR()
 {
-    printf '[\e[1;33mREPAIR\e[m] \e[1;35m%s\e[m \e[1;36m%s\e[m\n' "$COMMAND" "$2"
+    printf '[\e[1;33mREPAIR\e[m] \e[1;35m%s\e[m \e[1;36m%s\e[m\n' "$COMMAND" "$1"
 }
 PASS()
 {
-    printf '[\e[1;32mPASS\e[m] \e[1;35m%s\e[m \e[1;36m%s\e[m\n' "$COMMAND" "$2"
+    printf '[\e[1;32mPASS\e[m] \e[1;35m%s\e[m \e[1;36m%s\e[m\n' "$COMMAND" "$1"
 }
 FAIL()
 {
-    printf '[\e[1;31mPASS\e[m] \e[1;35m%s\e[m \e[1;36m%s\e[m\n' "$COMMAND" "$2"
+    printf '[\e[1;31mPASS\e[m] \e[1;35m%s\e[m \e[1;36m%s\e[m\n' "$COMMAND" "$1"
 }
 
 
