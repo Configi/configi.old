04070eaa62e68982e7c7c2cec71fedebfa51efc3
Eduardo Tongson <propolice@gmail.com>
2019-01-11 00:51:49 +0000

    exec,tests: Convert exec.spawn return value to (result, table).

--- vendor/lua/exec.lua
+++ vendor/lua/exec.lua
@@ -32,58 +32,87 @@ local redirect = function(io_or_filename, dest_fd)
   -- first check for regular
   if (io_or_filename == io.stdout or io_or_filename == STDOUT) and dest_fd ~= STDOUT then
     local r, e = dup2(STDERR, STDOUT)
-    if r == -1 then return nil, strerror(e, "dup2(2) failed")
+    if r == -1 then return nil, strerror(e, "dup2(2) failed") end
   elseif (io_or_filename == io.stderr or io_or_filename == STDERR) and dest_fd ~= STDERR then
     local r, e = dup2(STDOUT, STDERR)
-    if r == -1 then return nil, strerror(e, "dup2(2) failed")
+    if r == -1 then return nil, strerror(e, "dup2(2) failed") end
     -- otherwise handle file-based redirection
   else
     local fd, r, e
     fd, e = ffiext.open(io_or_filename)
-    if fd == -1 then return nil, strerror(e, "open(2) failed")
+    if fd == -1 then return nil, strerror(e, "open(2) failed") end
     r, e = dup2(fd, dest_fd)
-    if r == -1 then return nil, strerror(e, "dup2(2) failed")
+    if r == -1 then
+      C.close(fd)
+      return nil, strerror(e, "dup2(2) failed")
+    end
     C.close(fd)
   end
+  return true
 end
 
-exec.spawn = function (exe, args, env, cwd, stdin_string, stdout_redirect, stderr_redirect)
+exec.spawn = function (exe, args, env, cwd, stdin_string, stdout_redirect, stderr_redirect, ignore)
   args = args or {}
+  local R = {
+    stdout = {},
+    stderr = {}
+  }
   local ret
-  local stdout_tbl = {}
-  local stderr_tbl = {}
   local stdin = ffi.new("int[2]")
   local stdout = ffi.new("int[2]")
   local stderr = ffi.new("int[2]")
-  if C.pipe(stdin) == -1 then return nil, strerror(errno(), "pipe(2) for STDIN failed") end
-  if C.pipe(stdout) == -1 then return nil, strerror(errno(), "pipe(2) for STDOUT failed") end
-  if C.pipe(stderr) == -1 then return nil, strerror(errno(), "pipe(2) for STDERR failed") end
+  if C.pipe(stdin) == -1 then
+     R.error = strerror(errno(), "pipe(2) for STDIN failed")
+     return nil, R
+  end
+  if C.pipe(stdout) == -1 then
+     R.error = strerror(errno(), "pipe(2) for STDOUT failed")
+     return nil, R
+  end
+  if C.pipe(stderr) == -1 then
+     R.error = strerror(errno(), "pipe(2) for STDERR failed")
+     return nil, R
+  end
   local pid = C.fork()
   if pid < 0 then
-    return nil, strerror(errno(), "fork(2) failed")
+    R.error = strerror(errno(), "fork(2) failed")
+    return nil, R
   elseif pid == 0 then -- child process
     C.close(stdin[1])
     C.close(stdout[0])
     C.close(stderr[0])
     if stdin_string then
       local r, e = dup2(stdin[0], STDIN)
-      if r == -1 then return nil, nil, nil, strerror(e, "dup2(2) failed") end
+      if r == -1 then
+        R.error = strerror(e, "dup2(2) failed")
+        return nil, R
+      end
     end
     if stdout_redirect then
-      local r, e
-      r, e = redirect(stdout_redirect, STDOUT)
-      if r == -1 then return nil, nil, nil, sterror(e, "open(2) failed") end
+      local r, es = redirect(stdout_redirect, STDOUT)
+      if r == nil then
+        R.error = es
+        return nil, R
+      end
     else
-      r, e = dup2(stdout[1], STDOUT)
-      if r == -1 then return nil, nil, nil, strerror(e, "dup2(2) failed") end
+      local r, e = dup2(stdout[1], STDOUT)
+      if r == -1 then
+        R.error = strerror(e, "dup2(2) failed")
+        return nil, R
+      end
     end
     if stderr_redirect then
-      local r, e
-      r, e = redirect(stderr_redirect, STDERR)
-      if r == -1 then return nil, nil, nil, strerror(e, "dup2(2) failed") end
+      local r, es = redirect(stderr_redirect, STDERR)
+      if r == nil then
+        R.error = es
+        return nil, R
+      end
     else
-      r, e = dup2(stderr[1], STDERR)
-      if r == -1 then return nil, nil, nil, strerror(e, "dup2(2) failed") end
+      local r, e = dup2(stderr[1], STDERR)
+      if r == -1 then
+        R.error = strerror(e, "dup2(2) failed")
+        return nil, R
+      end
     end
     C.close(stdin[0])
     C.close(stdout[1])
@@ -106,17 +135,24 @@ exec.spawn = function (exe, args, env, cwd, stdin_string, stdout_redirect, stder
         end
       end
       for name, value in pairs(env or {}) do
-        local x, e = setenv(name, tostring(value))
-        if x == nil then return nil, e end
+        local x, es = setenv(name, tostring(value))
+        if x == nil then
+          R.error = es
+          return nil, R
+        end
       end
     end
     if cwd then
-      if C.chdir(tostring(cwd)) == -1 then return nil, nil, nil, strerror(errno(), "chdir(2) failed") end
+      if C.chdir(tostring(cwd)) == -1 then
+        R.error = strerror(errno(), "chdir(2) failed")
+        return nil, R
+      end
     end
     argv[0] = exe
     argv[#args + 1] = nil
     if C.execvp(exe, ffi.cast("char *const*", argv)) == -1 then
-      return nil, nil, nil, strerror(errno(), "execvp(2) failed")
+        R.error = strerror(errno(), "execvp(2) failed")
+        return nil, R
     end
     assert(nil, "assertion failed: exec.spawn (should be unreachable!)")
   else
@@ -132,7 +168,10 @@ exec.spawn = function (exe, args, env, cwd, stdin_string, stdout_redirect, stder
     do
       local status = ffi.new("int[?]", 1)
       local r, e = ffiext.retry(C.waitpid)(pid, status, 0)
-      if r == -1 then return nil, nil, nil, strerror(e, "waitpid(2) failed") end
+      if r == -1 then
+        R.error = strerror(e, "waitpid(2) failed")
+        return nil, R
+      end
       ret = bit.rshift(bit.band(status[0], 0xff00), 8)
     end
     local output = function(i, o)
@@ -168,9 +207,19 @@ exec.spawn = function (exe, args, env, cwd, stdin_string, stdout_redirect, stder
           return nil, strerror(errno(), "read(2) failed")
         end
       end
+      return true
     end
-    output(stdout[0], stdout_tbl)
-    output(stderr[0], stderr_tbl)
+    local sr, se
+    sr, se = output(stdout[0], R.stdout)
+    if sr == nil then
+      R.error = se
+      return nil, R
+    end
+    sr, se = output(stderr[0], R.stderr)
+    if sr == nil then
+      R.error = se
+      return nil, R
+    end
     C.close(stdin[0])
     C.close(stdin[1])
     C.close(stdout[0])
@@ -178,10 +227,10 @@ exec.spawn = function (exe, args, env, cwd, stdin_string, stdout_redirect, stder
     C.close(stderr[0])
     C.close(stderr[1])
   end
-  if ret == 0 then
-    return pid, stdout_tbl, stderr_tbl
+  if ret == 0 or ignore then
+    return pid, R
   else
-    return nil, stdout_tbl, stderr_tbl
+    return nil, R
   end
 end
 
@@ -198,7 +247,7 @@ exec.context = function(exe)
         args[#args+1] = k
       end
     end
-    return exec.spawn(exe, args, args.env, args.cwd, args.stdin, args.stdout, args.stderr)
+    return exec.spawn(exe, args, args.env, args.cwd, args.stdin, args.stdout, args.stderr, args.ignore)
   end})
 end
 exec.ctx = exec.context
@@ -215,7 +264,7 @@ exec.cmd = setmetatable({},
         else
           args = {...}
         end
-        return exec.spawn(exe, args, args.env, args.cwd, args.stdin, args.stdout, args.stderr)
+        return exec.spawn(exe, args, args.env, args.cwd, args.stdin, args.stdout, args.stderr, args.ignore)
       end
     end
   })
