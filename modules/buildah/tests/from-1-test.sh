d7426e35e2071bf81a2df6b15cb45bccf0166b83
Eduardo Tongson <propolice@gmail.com>
2019-01-12 00:11:40 +0000

    rerun.buildah.from: First buildah command.

---
+++ modules/buildah/tests/from-1-test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env roundup
+#
+#/ usage:  rerun stubbs:test -m buildah -p from [--answers <>]
+#
+
+# Helpers
+# -------
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+describe "from"
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
