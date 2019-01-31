0fcf11e932e5830f4deccb0f967c42db1ee3e238
Eduardo Tongson <propolice@gmail.com>
2019-01-31 10:29:14 +0000

    rerun.ecr: module for manipulating AWS ECR.

---
+++ tests/modules/ecr/tests/push-1-test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env roundup
+#
+#/ usage:  rerun stubbs:test -m ecr -p push [--answers <>]
+#
+
+# Helpers
+# -------
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+describe "push"
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
