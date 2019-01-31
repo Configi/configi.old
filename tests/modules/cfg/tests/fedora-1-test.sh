224b65cb39bf4ae5183d78f9a5f1bb842765949b
Eduardo Tongson <propolice@gmail.com>
2019-01-31 09:47:03 +0000

    rerun.cfg: New module for creating dummy containers which are going to be used to distribute the Configi executable, modules and manifest.

---
+++ tests/modules/cfg/tests/fedora-1-test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env roundup
+#
+#/ usage:  rerun stubbs:test -m cfg -p fedora [--answers <>]
+#
+
+# Helpers
+# -------
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+describe "fedora"
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
