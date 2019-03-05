33b5d778108ff489c185d91b771f777216a61ad6
Eduardo Tongson <propolice@gmail.com>
2019-03-05 04:53:03 +0000

    rerun.syslog_ng: Implement pulling container image.

---
+++ tests/modules/syslog_ng/tests/pull-1-test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env roundup
+#
+#/ usage:  rerun stubbs:test -m syslog_ng -p pull [--answers <>]
+#
+
+# Helpers
+# -------
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+describe "pull"
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
